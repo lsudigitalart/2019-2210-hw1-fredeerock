55cfe44f28d1e172943b40b50e9a6bfab90206fb
Derick Ostrenko <dostrenko@gmail.com>
2019-09-17 15:33:39 -0500

    circles

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 function setup() {
   createCanvas(800, 600);
-  ellipse(width / 2, height / 2, 100, 100);
-  ellipse(width / 2, height / 2, 10, 10);
+  ellipse(10, 10, 10, 10);
+  ellipse(20, 50, 100, 10);
 }
 
 function draw() {
-
+  // Put things you want to over and over here. 
 }
